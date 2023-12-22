4cf83ce7b11fef2bd360ee5a5aa0535e817d439b
Francisco Klaus <franciscoklausfilho@hotmail.com>
2023-12-22 16:55:30 -0300

    using scan to input values

--- 02 - Go essentials/investment_calculator.go
+++ 02 - Go essentials/investment_calculator.go
@@ -9,9 +9,18 @@ func main() {
 	//investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5 // multiple values with super short notation
 
 	const inflationRate = 6.5
-	var investmentAmount float64 = 1000
-	years := 10.0
-	expectedReturnRate := 5.5
+	var investmentAmount float64
+	var years float64
+	var expectedReturnRate float64
+
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
