d681e58ee426bc0d874de13340e3afb0a19f514b
Francisco Klaus <franciscoklausfilho@hotmail.com>
2023-12-22 16:38:11 -0300

    using constant

--- 02 - Go essentials/investment_calculator.go
+++ 02 - Go essentials/investment_calculator.go
@@ -6,8 +6,16 @@ import (
 )
 
 func main() {
-	investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5 // multiple values with super short notation
+	//investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5 // multiple values with super short notation
+
+	const inflationRate = 6.5
+	var investmentAmount float64 = 1000
+	years := 10.0
+	expectedReturnRate := 5.5
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
